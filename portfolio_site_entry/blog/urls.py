7c6f390bc69c986714fbf9a4b42219ae9adf3e2b
maxemrich <maxfisherbear@gmail.com>
2024-08-20 13:53:33 -0700

    finished prod

--- portfolio_site_entry/blog/urls.py
+++ portfolio_site_entry/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from .views import Index
-from .views import Index, DetailArticleView, LikeArticle
+from .views import Index, DetailArticleView, LikeArticle, Featured
 
 urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('', Index.as_view(), name='index'), # this is saying: when we go to the "" path on our webpage, go to the 'index' class in views and call that get index function  
     path('<int:pk>/', DetailArticleView.as_view(), name='detail_article'),
     path('<int:pk>/like', LikeArticle.as_view(), name='like_article'),
+    path('featured/', Featured.as_view(), name='featured'), 
 ]         
--- portfolio_site_entry/blog/views.py
+++ portfolio_site_entry/blog/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render, HttpResponse, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
@@ -8,11 +9,28 @@ class Index(ListView):
     model = Article
     queryset = Article.objects.all().order_by('-date')
     template_name = 'blog/index.html'
-    paginate_by = 1
+    paginate_by = 2
+
+class Featured(ListView):
+    model = Article
+    queryset = Article.objects.filter(featured=True).order_by('-date')
+    template_name = 'blog/featured.html'
+    paginate_by = 2
 
 class DetailArticleView(DetailView):
     model = Article
     template_name = 'blog/blog_post.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(DetailArticleView, self).get_context_data(*args, **kwargs)
+        context['liked_by_user'] = False
+        article = Article.objects.get(id=self.kwargs.get('pk'))
+        if article.likes.filter(pk=self.request.user.id).exists(): # does this person exit and has liked this post?
+            context['liked_by_user'] = True
+            return context
+        return context
+
+
 
 class LikeArticle(View):
     def post(self, request, pk):
