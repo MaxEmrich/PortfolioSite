4d1121955a208670d05a7709c01cd9997b55721f
Max Emrich <maxfisherbear@gmail.com>
2024-09-01 11:15:24 -0700

    nginx server setup

--- portfolio_site_entry/portfolio_site_entry/settings.py
+++ portfolio_site_entry/portfolio_site_entry/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-k$fcpm0*tg5nagzcifq$qt0)n5&(5ub&ah&phf=%#l)nt=8b(6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['talesfrom616.com', 'www.talesfrom616.com', '192.168.0.22']
 
 
 # Application definition
@@ -120,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
